3983891e6417ab2bc1aaa84ac0f08440dbec69f4
YABOIG <43472855+YABOIG@users.noreply.github.com>
2019-06-10 16:40:57 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const request = require('request');
-
-request('http://api.sl.se/api2/nearbystops.json?key=e80ab02e16f54c5bb29b681594962ba0&originCoordLat=59.32932349&originCoordLong=18.068580800000063', { json: true }, (err, res, body) => {
- if (err) { return console.log(err); }
- console.log(body.LocationList.StopLocation);
-});
